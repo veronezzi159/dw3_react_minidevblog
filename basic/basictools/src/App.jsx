3cc825f594e3d21d8ce231909db18cb4a06eaa70
hespanhol122 <hespanhol122@gmail.com>
2024-02-27 12:30:17 -0300

    aula2702 - Componentes de React

---
+++ basic/basictools/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Footer from './components/Footer/Footer'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+  return (
+    <> 
+      <NavBar></NavBar>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ basic/basictools/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import 'react'
+
+const NavBar = () =>{
+    return(
+        <>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Institucional</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Noticias</a></li>
+            </ul>
+        </nav>
+        </>
+    )
+}
+
+export default NavBar
